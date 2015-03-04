3ac44ff8b60cfc6f9d6ea90189da0e2b5000ae12
Javier Molina <jmolinaromero86@gmail.com>
2015-03-04 14:01:49 +0100

    Add Clase DaoFactory - Copiada y adaptada del ejercicio de clase

---
+++ src/main/java/es/upm/miw/models/daos/DaoFactory.java
@@ -0,0 +1,18 @@
+package es.upm.miw.models.daos;
+
+public abstract class DaoFactory {
+    public static DaoFactory factory = null;
+
+    public static void setFactory(DaoFactory factory) {
+        DaoFactory.factory = factory;
+    }
+
+    public static DaoFactory getFactory() {
+        assert factory != null;
+        return factory;
+    }
+
+    public abstract TemaDao getTemaDao();
+
+    public abstract VotacionDao getVotacionDao();
+}
