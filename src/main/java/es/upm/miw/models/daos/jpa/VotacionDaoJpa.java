437b8d7a81129dac7aae0e6b504675f298b29c80
Javier Molina <jmolinaromero86@gmail.com>
2015-03-05 10:44:28 +0100

    Add clase VotacionDaoJpa

--- src/main/java/es/upm/miw/models/daos/jpa/VotacionDaoJpa.java
+++ src/main/java/es/upm/miw/models/daos/jpa/VotacionDaoJpa.java
@@ -1,40 +1,13 @@
 package es.upm.miw.models.daos.jpa;
 
-import java.util.List;
-
 import es.upm.miw.models.daos.VotacionDao;
 import es.upm.miw.models.entities.Votacion;
 
-public class VotacionDaoJpa implements VotacionDao {
+public class VotacionDaoJpa extends GenericDaoJpa<Votacion, Integer> implements VotacionDao {
 
-	@Override
-	public void create(Votacion entity) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public Votacion read(Integer id) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public void update(Votacion entity) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void deleteById(Integer id) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public List<Votacion> findAll() {
-		// TODO Auto-generated method stub
-		return null;
+	public VotacionDaoJpa() {
+		super(Votacion.class);
+		// TODO Auto-generated constructor stub
 	}
 
 }
