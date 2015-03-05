af08f4b0b77588206ecd8f5609f48df89127f79a
Javier Molina <jmolinaromero86@gmail.com>
2015-03-05 13:54:27 +0100

    Final

--- src/test/java/es/upm/miw/models/daos/jpa/TemaDaoJpaTest.java
+++ src/test/java/es/upm/miw/models/daos/jpa/TemaDaoJpaTest.java
@@ -1,5 +1,20 @@
 package es.upm.miw.models.daos.jpa;
 
+import org.junit.Before;
+
+import es.upm.miw.models.daos.TemaDao;
+
 public class TemaDaoJpaTest {
+	
+	private TemaDao dao = DaoJpaFactory.getFactory().getTemaDao();
+	
+	private TemaDaoJpaTestData data;
+	
+	@Before
+	public voud init(){
+		data = new TemaDaoJpaTest();
+	}
+	
+	
 
 }
