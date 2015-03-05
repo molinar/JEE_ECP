3d89c8ce1fd3b0dd823f5668d0a74cb7e8587f29
Javier Molina <jmolinaromero86@gmail.com>
2015-03-05 13:15:09 +0100

    Update clase Votacion

--- src/main/java/es/upm/miw/models/entities/Votacion.java
+++ src/main/java/es/upm/miw/models/entities/Votacion.java
@@ -11,28 +11,28 @@ import es.upm.miw.models.utils.NivelEstudios;
 
 public class Votacion {
 	public static final String TABLE = "votacion";
-	
+
 	public static final String ID = "ID";
-	
-    @Id
-    @GeneratedValue
-    private Integer id;
-    
-    public static final String IP_USUARIO = "IP_USUARIO";
-	
+
+	@Id
+	@GeneratedValue
+	private Integer id;
+
+	public static final String IP_USUARIO = "IP_USUARIO";
+
 	private String ipUsuario;
-	
+
 	public static final String NIVEL_ESTUDIOS = "NIVEL_ESTUDIOS";
-	
-	@Enumerated(EnumType.STRING) 
-    private NivelEstudios nivelStudios;
-	
+
+	@Enumerated(EnumType.STRING)
+	private NivelEstudios nivelStudios;
+
 	public static final String VALORACION = "VALORACION";
-	
+
 	private Integer valoracion;
-	
+
 	public static final String TEMA = "ID_TEMA";
-	
+
 	@ManyToOne
 	@JoinColumn
 	private Tema tema;
@@ -77,19 +77,31 @@ public class Votacion {
 		this.tema = tema;
 	}
 
-
 	@Override
 	public boolean equals(Object obj) {
 		assert obj != null;
-        Votacion other = (Votacion) obj;
-        return id.equals(other.id) && ipUsuario.equals(other.ipUsuario)
-                && nivelStudios.equals(other.nivelStudios) && valoracion.equals(other.valoracion)
-                && tema.equals(other.tema);
+		Votacion other = (Votacion) obj;
+
+		return ipUsuario.equals(other.ipUsuario)
+				&& nivelStudios.equals(other.nivelStudios)
+				&& tema.equals(other.tema);
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((ipUsuario == null) ? 0 : ipUsuario.hashCode());
+		result = prime * result	+ ((nivelStudios == null) ? 0 : nivelStudios.hashCode());
+		result = prime * result + valoracion;
+		result = prime * result + ((tema == null) ? 0 : tema.hashCode());
+		return result;
 	}
 
 	@Override
 	public String toString() {
-		return "Votacion [id=" + id + ", ip usuario=" + ipUsuario + ", nivel de estudios=" 
-				+ nivelStudios + ", valoracion=" + valoracion + ", id tema=" + tema.getId() +"]";
-	}	
+		return "Votacion [id=" + id + ", ip usuario=" + ipUsuario
+				+ ", nivel de estudios=" + nivelStudios + ", valoracion="
+				+ valoracion + ", id tema=" + tema.getId() + "]";
+	}
 }
