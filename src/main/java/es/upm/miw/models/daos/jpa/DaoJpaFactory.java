872a27e954d80e4ae44a74fecec08568ef798e46
molinar <jmolinaromero86@gmail.com>
2015-03-09 19:55:33 +0100

    Update clase DaoJpaFactory

--- src/main/java/es/upm/miw/models/daos/jpa/DaoJpaFactory.java
+++ src/main/java/es/upm/miw/models/daos/jpa/DaoJpaFactory.java
@@ -1,10 +1,11 @@
 package es.upm.miw.models.daos.jpa;
 
+import java.util.HashMap;
+import java.util.Map;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
-
 import org.apache.logging.log4j.LogManager;
-
+import org.eclipse.persistence.config.PersistenceUnitProperties;
 import es.upm.miw.models.daos.DaoFactory;
 import es.upm.miw.models.daos.TemaDao;
 import es.upm.miw.models.daos.VotacionDao;
@@ -12,27 +13,35 @@ import es.upm.miw.models.daos.VotacionDao;
 public class DaoJpaFactory extends DaoFactory{
 	
 	private static final String PERSISTENCE_UNIT = "JEE_ECP";
-
-	private static EntityManagerFactory entityManagerFactory = Persistence
-            .createEntityManagerFactory(PERSISTENCE_UNIT);
 	
-	public DaoJpaFactory() {
-        LogManager.getLogger(DaoJpaFactory.class).debug("create Entity Manager Factory");
+	private static EntityManagerFactory entityManagerFactory; 
+
+    public DaoJpaFactory() {
     }
 
     public static EntityManagerFactory getEntityManagerFactory() {
+        if (entityManagerFactory == null) {
+            entityManagerFactory = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT);
+            LogManager.getLogger(DaoJpaFactory.class).debug("create Entity Manager Factory");
+        }
         return entityManagerFactory;
+    }
+
+    public static void prepareFactoryWithDropAndCreateTables() {
+        Map<String, String> properties = new HashMap<>();
+        properties.put(PersistenceUnitProperties.DDL_GENERATION,
+                PersistenceUnitProperties.DROP_AND_CREATE);
+        entityManagerFactory = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT, properties);
+        LogManager.getLogger(DaoJpaFactory.class).debug("create Entity Manager Factory");
     }
 
 	@Override
 	public TemaDao getTemaDao() {
-		// TODO Auto-generated method stub
 		return new TemaDaoJpa();
 	}
 
 	@Override
 	public VotacionDao getVotacionDao() {
-		// TODO Auto-generated method stub
 		return new VotacionDaoJpa();
 	}
 
