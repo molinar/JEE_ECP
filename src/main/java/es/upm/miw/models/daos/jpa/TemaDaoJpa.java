0d405253ca78f5e51a03c8b56b45102bdd1e329a
molinar <jmolinaromero86@gmail.com>
2015-03-11 20:16:44 +0100

    Update clase TemaDaoJpa

--- src/main/java/es/upm/miw/models/daos/jpa/TemaDaoJpa.java
+++ src/main/java/es/upm/miw/models/daos/jpa/TemaDaoJpa.java
@@ -1,12 +1,25 @@
 package es.upm.miw.models.daos.jpa;
 
+import es.upm.miw.models.daos.DaoFactory;
 import es.upm.miw.models.daos.TemaDao;
+import es.upm.miw.models.daos.VotacionDao;
 import es.upm.miw.models.entities.Tema;
 
 public class TemaDaoJpa extends GenericDaoJpa<Tema, Integer> implements TemaDao {
 
-	public TemaDaoJpa() {
+    public TemaDaoJpa() {
 		super(Tema.class);
 		// TODO Auto-generated constructor stub
-	}	
+	}
+
+    @Override //Comprobar
+    public void deleteById(Integer id) {
+        Tema tema = this.read(id);
+        if(tema != null){
+            DaoFactory.setFactory(new DaoJpaFactory());
+            VotacionDao dao = DaoJpaFactory.getFactory().getVotacionDao();
+            dao.deleteVotosTema(tema);
+            super.deleteById(id);    
+        } 
+    }	
 }
