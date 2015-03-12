82d022218317ee18b6b3ff8c915f8c26f2766ecb
molinar <jmolinaromero86@gmail.com>
2015-03-12 11:39:32 +0100

    Update TemaDaoJpa

--- src/main/java/es/upm/miw/models/daos/jpa/TemaDaoJpa.java
+++ src/main/java/es/upm/miw/models/daos/jpa/TemaDaoJpa.java
@@ -1,5 +1,9 @@
 package es.upm.miw.models.daos.jpa;
 
+import javax.persistence.EntityManager;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaDelete;
+
 import es.upm.miw.models.daos.DaoFactory;
 import es.upm.miw.models.daos.TemaDao;
 import es.upm.miw.models.daos.VotacionDao;
@@ -12,7 +16,7 @@ public class TemaDaoJpa extends GenericDaoJpa<Tema, Integer> implements TemaDao 
 		// TODO Auto-generated constructor stub
 	}
 
-    @Override //Revisar, algo va mal
+    @Override
     public void deleteById(Integer id) {
         Tema tema = this.read(id);
         if(tema != null){
@@ -21,5 +25,15 @@ public class TemaDaoJpa extends GenericDaoJpa<Tema, Integer> implements TemaDao 
             dao.deleteVotosTema(tema);
             super.deleteById(id);    
         } 
+    }
+
+    @Override
+    public void deleteTodosTemas() {
+        EntityManager entityManager = DaoJpaFactory.getEntityManagerFactory().createEntityManager();
+        CriteriaBuilder criteria = entityManager.getCriteriaBuilder();
+        CriteriaDelete<Tema> delete = criteria.createCriteriaDelete(Tema.class);
+        entityManager.getTransaction().begin();
+        entityManager.createQuery(delete).executeUpdate();
+        entityManager.getTransaction().commit();
     }	
 }
