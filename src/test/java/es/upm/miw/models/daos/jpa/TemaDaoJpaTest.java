cf2ed98535d01e48d03936c859e57ef146561684
molinar <jmolinaromero86@gmail.com>
2015-03-12 11:16:44 +0100

    Update TemaDaoJpaTest

--- src/test/java/es/upm/miw/models/daos/jpa/TemaDaoJpaTest.java
+++ src/test/java/es/upm/miw/models/daos/jpa/TemaDaoJpaTest.java
@@ -20,6 +20,7 @@ public class TemaDaoJpaTest {
 	private VotacionDao votacionDao = DaoJpaFactory.getFactory().getVotacionDao();
 	
 	private Tema tema;
+	private Votacion v1, v2, v3;
 	
 	@BeforeClass 
 	public static void beforeClass(){
@@ -31,21 +32,23 @@ public class TemaDaoJpaTest {
 	public void init(){
 	    this.tema = new Tema("Tema 1", "Pregunta 1");
 	    dao.create(this.tema);
-	    Votacion v1 = new Votacion("ip1", NivelEstudios.EDUCACION_PRIMARIA, 3, tema);
-	    Votacion v2 = new Votacion("ip2", NivelEstudios.ESTUDIOS_DOCTORADO, 5, tema);
-	    Votacion v3 = new Votacion("ip3", NivelEstudios.EDUCACION_SECUNDARIA, 4, tema);
+	    this.v1 = new Votacion("ip1", NivelEstudios.EDUCACION_PRIMARIA, 3, tema);
+	    this.v2 = new Votacion("ip2", NivelEstudios.ESTUDIOS_DOCTORADO, 5, tema);
+	    this.v3 = new Votacion("ip3", NivelEstudios.EDUCACION_SECUNDARIA, 4, tema);
 	    votacionDao.create(v1);
 	    votacionDao.create(v2);
 	    votacionDao.create(v3);
 	}
 	
-/*	@Test
+	@Test
 	public void testCreateTema(){
         Tema t2 = new Tema("Tema 2", "Pregunta 2");
         dao.create(t2);
+        Votacion v4 = new Votacion("ip4", NivelEstudios.EDUCACION_SECUNDARIA, 4, t2);
+        votacionDao.create(v4);
 		assertEquals(t2, dao.read(t2.getId()));
 	}
-	
+		
 	@Test
 	public void testReadTema() {
 	    assertEquals(this.tema, dao.read(this.tema.getId()));
@@ -56,22 +59,25 @@ public class TemaDaoJpaTest {
 	    this.tema.setNombre("Primer Tema");
 	    dao.update(this.tema);
 	    assertEquals(this.tema.getNombre(), dao.read(this.tema.getId()).getNombre());		
-	}*/
+	}
 	
 	@Test
 	public void testDeleteTemaByID() {
 		dao.deleteById(this.tema.getId());
 		assertNull(dao.read(this.tema.getId()));
+		assertNull(votacionDao.read(this.v1.getId()));
+		assertNull(votacionDao.read(this.v2.getId()));
+		assertNull(votacionDao.read(this.v3.getId()));
 	}
-/*	
+	
 	@Test
     public void testFindAll() {
 	    dao.create(new Tema("Tema 3", "Pregunta 3"));
 	    assertEquals(2, dao.findAll().size());
 	}
-	*/
+	
 	@After
-	public void after(){
+	public void after(){ //Mirar cómo borrar las tablas;
         DaoJpaFactory.prepareFactoryWithDropAndCreateTables();
 	}
 	
