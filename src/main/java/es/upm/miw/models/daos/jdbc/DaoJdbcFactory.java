586398d53369e704f991d3f720d4bbc58dcf5e97
molinar <jmolinaromero86@gmail.com>
2015-03-13 11:35:35 +0100

    Add DaoJdbcFactory. Copiado del ejercicio de persistencia de clase.

--- src/main/java/es/upm/miw/models/daos/jdbc/DaoJdbcFactory.java
+++ src/main/java/es/upm/miw/models/daos/jdbc/DaoJdbcFactory.java
@@ -1,5 +1,63 @@
 package es.upm.miw.models.daos.jdbc;
 
-public class DaoJdbcFactory {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 
+import org.apache.logging.log4j.LogManager;
+
+import es.upm.miw.models.daos.DaoFactory;
+import es.upm.miw.models.daos.TemaDao;
+import es.upm.miw.models.daos.VotacionDao;
+
+public class DaoJdbcFactory extends DaoFactory{
+    private static final String DRIVER = "com.mysql.jdbc.Driver";
+
+    private static final String URL = "jdbc:mysql://localhost:3306/jee";
+
+    private static final String USER = "root";
+
+    private static final String PASS = "";
+
+    private static final String DROP_TABLE = "DROP TABLE IF EXISTS %s";
+
+    private static Connection connection;
+
+    public static Connection getConnection() {
+        try {
+            if (connection == null || !connection.isValid(0)) {
+                Class.forName(DRIVER);
+                connection = DriverManager.getConnection(URL, USER, PASS);
+            }
+        } catch (ClassNotFoundException e) {
+            LogManager.getLogger(DaoJdbcFactory.class).error(
+                    "Problemas con el driver: " + e.getMessage());
+        } catch (SQLException e) {
+            LogManager.getLogger(DaoJdbcFactory.class).error(
+                    "Problemas con la BD: " + e.getMessage());
+        }
+        return connection;
+    }
+
+    public static void dropAndCreateTables() {
+        try {
+            Statement statement = getConnection().createStatement();
+
+        } catch (SQLException e) {
+            LogManager.getLogger(DaoJdbcFactory.class).error("Drop tables: " + e.getMessage());
+        }
+    }
+
+    @Override
+    public TemaDao getTemaDao() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public VotacionDao getVotacionDao() {
+        // TODO Auto-generated method stub
+        return null;
+    }
 }
