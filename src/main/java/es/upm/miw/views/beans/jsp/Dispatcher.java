eed3e6a77d3a840b3ac0a910dd9e36364f7ccfb4
molinar <jmolinaromero86@gmail.com>
2015-03-15 11:46:49 +0100

    Add Dispatcher.java

--- src/main/java/es/upm/miw/views/beans/jsp/Dispatcher.java
+++ src/main/java/es/upm/miw/views/beans/jsp/Dispatcher.java
@@ -27,6 +27,15 @@ public class Dispatcher extends HttpServlet {
             request.setAttribute(action, verTemasView);
             view = action;
             break;
+        case "verVotos":
+            
+            view = action;
+            break;
+            
+        case "borrarTema":
+            
+            view = action;
+            break;
         default:
             view = "home";
         }
@@ -41,25 +50,14 @@ public class Dispatcher extends HttpServlet {
             throws ServletException, IOException {
         String action = request.getPathInfo().substring(1);
         String view = "home";
-        /*   switch (action) {
-        case "persona":
-            Persona persona = new Persona();
-            persona.setId(Integer.valueOf(request.getParameter("id")));
-            persona.setNombre(request.getParameter("nombre"));
-            persona.setRol(request.getParameter("rol"));
-            PersonaView personaView = new PersonaView(;
-            personaView.setPersona(persona);
-            request.setAttribute(action, personaView);
-            view = personaView.process();
-            break;
-        case "rol":
-            RolView rolView = new RolView();
-            rolView.setRol(request.getParameter("rol"));
-            request.setAttribute(action, rolView);
-            view = rolView.process();
-            break;
-        }*/
+        switch (action) {
+        case "votar":
 
+            break;
+        case "agregarTema":
+
+            break;
+        }
         this.getServletContext().getRequestDispatcher(PATH_ROOT_VIEW + view + ".jsp")
                 .forward(request, response);
     }
