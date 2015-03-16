85593a1fbd58a1881235c4737b539e45a3e89fd4
molinar <jmolinaromero86@gmail.com>
2015-03-16 20:21:15 +0100

    Add EliminarTemaControllerEjb

--- src/main/java/es/upm/miw/controllersEjb/EliminarTemaControllerEjb.java
+++ src/main/java/es/upm/miw/controllersEjb/EliminarTemaControllerEjb.java
@@ -1,7 +1,15 @@
 package es.upm.miw.controllersEjb;
 
 import es.upm.miw.controllers.EliminarTemaController;
+import es.upm.miw.models.daos.TemaDao;
+import es.upm.miw.models.daos.jpa.DaoJpaFactory;
 
-public class EliminarTemaControllerEjb extends EliminarTemaController {
+public class EliminarTemaControllerEjb implements EliminarTemaController {
+
+    @Override
+    public void eliminarTema(Integer id) {
+        TemaDao temaDao = DaoJpaFactory.getFactory().getTemaDao();
+        temaDao.deleteById(id);
+    }
 
 }
