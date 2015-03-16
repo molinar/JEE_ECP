16e7f944c5d59ffb5cf0b880dda10976cda71072
molinar <jmolinaromero86@gmail.com>
2015-03-16 20:53:32 +0100

    Final

---
+++ src/main/java/es/upm/miw/views/beans/jsp/AgregarTemaView.java
@@ -0,0 +1,10 @@
+package es.upm.miw.views.beans.jsp;
+
+public class AgregarTemaView extends ViewBean {
+
+    public String process() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
--- src/main/java/es/upm/miw/views/beans/jsp/Dispatcher.java
+++ src/main/java/es/upm/miw/views/beans/jsp/Dispatcher.java
@@ -8,65 +8,70 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import es.upm.miw.models.entities.Tema;
+
+import es.upm.miw.controllers.*;
+
 @WebServlet("/jsp/*")
 public class Dispatcher extends HttpServlet {
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	private static String PATH_ROOT_VIEW = "/pages/jsp/";
+    private static String PATH_ROOT_VIEW = "/pages/jsp/";
+    
+    private ControllerFactory controllerFactory;
 
-	@Override
-	protected void doGet(HttpServletRequest request,
-			HttpServletResponse response) throws ServletException, IOException {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
 
-		String view = "home";
-		System.out.println(request.getPathInfo());
-		request.setCharacterEncoding("UTF-8");
-		if (request.getPathInfo() != null) {
-			String action = request.getPathInfo().substring(1);
-			switch (action) {
-			case "verTemas":
-				VerTemasView verTemasView = new VerTemasView();
-				request.setAttribute(action, verTemasView);
-				view = action;
-				break;
-			/*
-			 * case "verVotos":
-			 * 
-			 * view = action; break;
-			 * 
-			 * case "borrarTema":
-			 * 
-			 * view = action; break;
-			 */
-			default:
-				view = "home";
-			}
+        String view = "home";
+        request.setCharacterEncoding("UTF-8");
+        if (request.getPathInfo() != null) {
+            String action = request.getPathInfo().substring(1);
+            switch (action) {
+/*
+             * case "verVotos":
+             * 
+             * view = action; break;
+             * 
+             * case "borrarTema":
+             * 
+             * view = action; break;
+             */
+            default:
+                view = "home";
+            }
 
-		}
+        }
 
-		this.getServletContext()
-				.getRequestDispatcher(PATH_ROOT_VIEW + view + ".jsp")
-				.forward(request, response);
+        this.getServletContext().getRequestDispatcher(PATH_ROOT_VIEW + view + ".jsp")
+                .forward(request, response);
 
-	}
+    }
 
-	@Override
-	protected void doPost(HttpServletRequest request,
-			HttpServletResponse response) throws ServletException, IOException {
-		String action = request.getPathInfo().substring(1);
-		String view = "home";
-		request.setCharacterEncoding("UTF-8");
-		switch (action) {
-		case "votar":
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String action = request.getPathInfo().substring(1);
+        String view = "home";
+        request.setCharacterEncoding("UTF-8");
+        switch (action) {
+        case "votar":
 
-			break;
-		case "agregarTema":
+            break;
+        case "agregarTema":
+            Tema tema = new Tema();
+            tema.setNombre(request.getParameter("nombre"));
+            tema.setPregunta(request.getParameter("pregunta"));
+            AgregarTemaView agregarTemaView = new AgregarTemaView();
+            agregarTemaView.setControllerFactory(controllerFactory);
+            request.setAttribute(action, agregarTemaView);
+            view = agregarTemaView.process();
 
-			break;
-		}
-		this.getServletContext()
-				.getRequestDispatcher(PATH_ROOT_VIEW + view + ".jsp")
-				.forward(request, response);
-	}
+            break;
+        }
+        this.getServletContext().getRequestDispatcher(PATH_ROOT_VIEW + view + ".jsp")
+                .forward(request, response);
+    }
 
 }
