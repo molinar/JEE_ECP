39f9afb7133a20e79d698622290869d6f76cf879
molinar <jmolinaromero86@gmail.com>
2015-03-17 20:38:35 +0100

    Update agregarTema.jsp

--- src/main/java/es/upm/miw/views/beans/jsp/AgregarTemaView.java
+++ src/main/java/es/upm/miw/views/beans/jsp/AgregarTemaView.java
@@ -7,33 +7,32 @@ import es.upm.miw.models.entities.Tema;
 
 public class AgregarTemaView extends ViewBean {
 
-	private String errorMsg;
+    private String errorMsg;
 
-	private Tema tema;
+    private Tema tema;
 
-	public AgregarTemaView() {
-		this.tema = new Tema();
-	}
+    public AgregarTemaView() {
+        this.tema = new Tema();
+    }
 
-	public String getErrorMsg() {
-		return errorMsg;
-	}
+    public String getErrorMsg() {
+        return errorMsg;
+    }
 
-	public Tema getTema() {
-		return tema;
-	}
+    public Tema getTema() {
+        return tema;
+    }
 
-	public void setTema(Tema tema) {
-		this.tema = tema;
-	}
+    public void setTema(Tema tema) {
+        this.tema = tema;
+    }
 
-	public String agregarTema() {
-		AgregarTemaController agregarTemaController = this
-				.getControllerFactory().getAgregarTemaController();
-		System.out.println("bean: " + tema.getNombre());
-		agregarTemaController.agregarTema(tema);
-		LogManager.getLogger(this.getClass().getName()).info(
-				"--- Tema creado ---");
-		return null;
-	}
+    public String agregarTema() {
+        AgregarTemaController agregarTemaController = this.getControllerFactory()
+                .getAgregarTemaController();
+        System.out.println("bean: " + tema.getNombre());
+        agregarTemaController.agregarTema(tema);
+        LogManager.getLogger(this.getClass().getName()).info("--- Tema creado ---");
+        return null;
+    }
 }
