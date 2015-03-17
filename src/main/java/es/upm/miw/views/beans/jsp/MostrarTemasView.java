5fa60c992e42fcf9c6e2a278a34068c32a90bb86
molinar <jmolinaromero86@gmail.com>
2015-03-17 19:58:25 +0100

    Update MostrarTemasView

--- src/main/java/es/upm/miw/views/beans/jsp/MostrarTemasView.java
+++ src/main/java/es/upm/miw/views/beans/jsp/MostrarTemasView.java
@@ -1,41 +1,32 @@
 package es.upm.miw.views.beans.jsp;
 
+import java.util.List;
+
 import org.apache.logging.log4j.LogManager;
 
 import es.upm.miw.controllers.EliminarTemaController;
 import es.upm.miw.models.entities.Tema;
 
-public class MostrarTemasView extends ViewBean{
+public class MostrarTemasView extends ViewBean {
 
-    private String errorMsg;
-
-    private Tema tema;
+    private List<Tema> temas;
 
     public MostrarTemasView() {
-        this.tema = new Tema();
     }
 
-    public String getErrorMsg() {
-        return errorMsg;
+    public List<Tema> getTemas() {
+        return temas;
     }
 
-    public Tema getTema() {
-        return tema;
+    public void setTemas(List<Tema> temas) {
+        this.temas = temas;
     }
 
-    public void setErrorMsg(String errorMsg) {
-        this.errorMsg = errorMsg;
-    }
-
-    public void setTema(Tema tema) {
-        this.tema = tema;
-    }
-    
-    public String mostrarTemas() {
-        EliminarTemaController eliminarTemaController = this.getControllerFactory().getEliminarTemaController();
-        eliminarTemaController.listarTemas();
+    public void mostrarTemas() {
+        EliminarTemaController eliminarTemaController = this.getControllerFactory()
+                .getEliminarTemaController();
+        temas = eliminarTemaController.listarTemas();
         LogManager.getLogger(this.getClass().getName()).info("--- Temas mostrados ---");
-        return null;
     }
 
 }
