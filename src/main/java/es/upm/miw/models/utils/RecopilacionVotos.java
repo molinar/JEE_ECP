d72b2883f2671872dd4eccd527e473bad3498efd
Javier Molina <jmolinaromero86@gmail.com>
2015-03-17 10:44:10 +0100

    Add clase RecopilacionVotos

---
+++ src/main/java/es/upm/miw/models/utils/RecopilacionVotos.java
@@ -0,0 +1,39 @@
+package es.upm.miw.models.utils;
+
+import java.util.List;
+
+import es.upm.miw.models.entities.Tema;
+
+public class RecopilacionVotos {
+
+	private Tema tema;
+
+	private int numeroVotos;
+
+	private List<RecopilacionMediasTema> recopilacionMediasTema;
+
+	public Tema getTema() {
+		return tema;
+	}
+
+	public void setTema(Tema tema) {
+		this.tema = tema;
+	}
+
+	public int getNumeroVotos() {
+		return numeroVotos;
+	}
+
+	public void setNumeroVotos(int numeroVotos) {
+		this.numeroVotos = numeroVotos;
+	}
+
+	public List<RecopilacionMediasTema> getRecopilacionMediasTema() {
+		return recopilacionMediasTema;
+	}
+
+	public void setRecopilacionMediasTema(
+			List<RecopilacionMediasTema> recopilacionMediasTema) {
+		this.recopilacionMediasTema = recopilacionMediasTema;
+	}
+}
