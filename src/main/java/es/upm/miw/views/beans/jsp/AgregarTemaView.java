f94fd4fbcc1c77e213c8a7bc8c36a8201e4edc38
Javier Molina <jmolinaromero86@gmail.com>
2015-03-17 12:53:11 +0100

    Add AgregarTemaView

--- src/main/java/es/upm/miw/views/beans/jsp/AgregarTemaView.java
+++ src/main/java/es/upm/miw/views/beans/jsp/AgregarTemaView.java
@@ -1,10 +1,38 @@
 package es.upm.miw.views.beans.jsp;
 
+import org.apache.logging.log4j.LogManager;
+
+import es.upm.miw.controllers.AgregarTemaController;
+import es.upm.miw.models.entities.Tema;
+
 public class AgregarTemaView extends ViewBean {
 
-    public String process() {
-        // TODO Auto-generated method stub
-        return null;
-    }
+	private String errorMsg;
 
+	private Tema tema;
+
+	public AgregarTemaView() {
+
+	}
+
+	public String getErrorMsg() {
+		return errorMsg;
+	}
+
+	public Tema getTema() {
+		return tema;
+	}
+
+	public void setTema(Tema tema) {
+		this.tema = tema;
+	}
+
+	public String agregarTema() {
+		AgregarTemaController agregarTemaController = this
+				.getControllerFactory().getAgregarTemaController();
+		agregarTemaController.agregarTema(tema);
+		LogManager.getLogger(this.getClass().getName()).info(
+				"--- Tema creado ---");
+		return null;
+	}
 }
