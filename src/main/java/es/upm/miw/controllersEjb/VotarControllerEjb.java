1016d7c0b035b73430cf54410984c7c45e979b56
Javier Molina <jmolinaromero86@gmail.com>
2015-03-17 09:50:54 +0100

    Add VotarControllerEjb

--- src/main/java/es/upm/miw/controllersEjb/VotarControllerEjb.java
+++ src/main/java/es/upm/miw/controllersEjb/VotarControllerEjb.java
@@ -1,7 +1,16 @@
 package es.upm.miw.controllersEjb;
 
 import es.upm.miw.controllers.VotarController;
+import es.upm.miw.models.daos.VotacionDao;
+import es.upm.miw.models.daos.jpa.DaoJpaFactory;
+import es.upm.miw.models.entities.Votacion;
 
-public class VotarControllerEjb extends VotarController {
+public class VotarControllerEjb extends ListarTemasControllerEjb implements VotarController {
+
+	@Override
+	public void votar(Votacion votacion) {
+		VotacionDao votacionDao = DaoJpaFactory.getFactory().getVotacionDao();
+		votacionDao.create(votacion);
+	}
 
 }
