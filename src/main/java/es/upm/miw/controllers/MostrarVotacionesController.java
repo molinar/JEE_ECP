5110ff2b6ebb4aa0ab08a7821718959f873ebf67
Javier Molina <jmolinaromero86@gmail.com>
2015-03-17 10:00:47 +0100

    Add MostrarVotacionesController

--- src/main/java/es/upm/miw/controllers/MostrarVotacionesController.java
+++ src/main/java/es/upm/miw/controllers/MostrarVotacionesController.java
@@ -1,5 +1,11 @@
 package es.upm.miw.controllers;
 
-public class MostrarVotacionesController {
+import java.util.List;
+
+import es.upm.miw.models.entities.Votacion;
+
+public interface MostrarVotacionesController extends ListarTemasController{
+	
+	List<Votacion> listarVotaciones();
 
 }
