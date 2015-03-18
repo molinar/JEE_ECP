884a740baa6454e94efae83ba2e8030b89402cbe
molinar <jmolinaromero86@gmail.com>
2015-03-18 18:21:52 +0100

    Add MostrarTemasVotarView

---
+++ src/main/java/es/upm/miw/views/beans/jsp/MostrarTemasVotarView.java
@@ -0,0 +1,31 @@
+package es.upm.miw.views.beans.jsp;
+
+import java.util.List;
+
+import org.apache.logging.log4j.LogManager;
+
+import es.upm.miw.controllers.VotarController;
+import es.upm.miw.models.entities.Tema;
+
+public class MostrarTemasVotarView extends ViewBean {
+    
+    private List<Tema> temas;
+    
+    public MostrarTemasVotarView(){
+    }
+    
+    public List<Tema> getTemas() {
+        return temas;
+    }
+
+    public void setTemas(List<Tema> temas) {
+        this.temas = temas;
+    }
+
+    public void mostrarTemas() {
+        VotarController votarController = this.getControllerFactory().getVotarController();
+        temas = votarController.listarTemas();
+        LogManager.getLogger(this.getClass().getName()).info("--- Temas mostrados ---");
+    }
+
+}
