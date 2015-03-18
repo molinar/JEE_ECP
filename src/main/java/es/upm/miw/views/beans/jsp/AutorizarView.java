910fc980fa3a1e9fa4add797dc092990d9c429a1
Javier Molina <jmolinaromero86@gmail.com>
2015-03-18 10:23:17 +0100

    Add AutorizarView

--- src/main/java/es/upm/miw/views/beans/jsp/AutorizarView.java
+++ src/main/java/es/upm/miw/views/beans/jsp/AutorizarView.java
@@ -1,5 +1,24 @@
 package es.upm.miw.views.beans.jsp;
 
-public class AutorizarView {
+import org.apache.logging.log4j.LogManager;
 
+import es.upm.miw.controllers.EliminarTemaController;
+
+public class AutorizarView extends ViewBean{
+	
+	String codigo;
+
+	public String getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(String codigo) {
+		this.codigo = codigo;
+	}
+
+	public void autorizar() {
+		EliminarTemaController eliminarTemaController = this.getControllerFactory().getEliminarTemaController();
+		eliminarTemaController.autorizar(codigo);
+		LogManager.getLogger(this.getClass().getName()).info("--- Autorización pedida ---");
+	}
 }
