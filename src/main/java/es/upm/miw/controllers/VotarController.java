ca5975b6b162e506cb1078de8a39f85b26a93866
molinar <jmolinaromero86@gmail.com>
2015-03-19 11:56:38 +0100

    Update VotarController

--- src/main/java/es/upm/miw/controllers/VotarController.java
+++ src/main/java/es/upm/miw/controllers/VotarController.java
@@ -1,8 +1,11 @@
 package es.upm.miw.controllers;
 
+import es.upm.miw.models.entities.Tema;
 import es.upm.miw.models.entities.Votacion;
 
 public interface VotarController extends ListarTemasController {
 
 	void votar(Votacion votacion);
+
+    Tema getTema();
 }
