718ddc03d051e173d0b16d7818bdf2da0cb050ff
molinar <jmolinaromero86@gmail.com>
2015-03-20 18:58:55 +0100

    Update bean VotarTemaView

--- src/main/java/es/upm/miw/views/beans/jsp/VotarTemaView.java
+++ src/main/java/es/upm/miw/views/beans/jsp/VotarTemaView.java
@@ -1,56 +1,64 @@
 package es.upm.miw.views.beans.jsp;
 
+import java.util.ArrayList;
+import java.util.EnumSet;
+import java.util.List;
+
 import es.upm.miw.controllers.VotarController;
 import es.upm.miw.models.entities.Tema;
 import es.upm.miw.models.utils.NivelEstudios;
 
-public class VotarTemaView extends ViewBean{
-    
+public class VotarTemaView extends ViewBean {
+
     public Tema tema;
-    
-    public NivelEstudios[] nivelEstudios;
-    
-    public int valoracion, id;
-    
-    public VotarTemaView(){
-        this.nivelEstudios = NivelEstudios.values();
+
+    public int id;
+
+    public List<NivelEstudios> nivelEstudios = new ArrayList<NivelEstudios>(EnumSet.allOf(NivelEstudios.class));
+
+    public List<Integer> valoracion = new ArrayList<Integer>();
+
+    public VotarTemaView() {
     }
 
     public Tema getTema() {
         return tema;
     }
 
-    public NivelEstudios[] getNivelEstudios() {
-        return nivelEstudios;
-    }
-
-    public int getValoracion() {
-        return valoracion;
-    }
-
     public void setTema(Tema tema) {
         this.tema = tema;
     }
 
-    public void setNivelEstudios(NivelEstudios[] nivelEstudios) {
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public List<NivelEstudios> getNivelEstudios() {
+        return nivelEstudios;
+    }
+
+    public List<Integer> getValoracion() {
+        for(int i=1; i<=10; i++){
+            valoracion.add(i);
+        }
+        return valoracion;
+    }
+
+    public void setNivelEstudios(List<NivelEstudios> nivelEstudios) {
         this.nivelEstudios = nivelEstudios;
     }
 
-    public void setValoracion(int valoracion) {
+    public void setValoracion(List<Integer> valoracion) {
         this.valoracion = valoracion;
     }
-    
-    public void setId(int id){
-        this.id = id;
-    }
-    
-    public void update(){
+
+    public void mostrar() {
         VotarController votarController = this.getControllerFactory().getVotarController();
         this.tema = votarController.getTema(id);
     }
-    
-    public void votarTema(){
-        
+
+    public void votarTema() {
+
     }
 
 }
