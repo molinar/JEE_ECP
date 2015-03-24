2fc489cddb621d4d10c875eea8faf6306f319fa3
molinar <jmolinaromero86@gmail.com>
2015-03-24 20:45:44 +0100

    Update bean EliminarTemaView - ElminarTema JSF correcto

--- src/main/java/es/upm/miw/views/beans/jsp/EliminarTemaView.java
+++ src/main/java/es/upm/miw/views/beans/jsp/EliminarTemaView.java
@@ -1,33 +1,35 @@
 package es.upm.miw.views.beans.jsp;
 
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
 
 import org.apache.logging.log4j.LogManager;
 
 import es.upm.miw.controllers.EliminarTemaController;
 
 @ManagedBean
-public class EliminarTemaView extends ViewBean{
-	
-	private int id;
+public class EliminarTemaView extends ViewBean {
 
-	public int getId() {
-		return id;
-	}
+    @ManagedProperty(value = "#{param.id}")
+    private int id;
 
-	public void setId(int id) {
-		this.id = id;
-	}
-	
+    public EliminarTemaView() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
     public String eliminarTema() {
-    	EliminarTemaController eliminarTemaController = this.getControllerFactory()
+        EliminarTemaController eliminarTemaController = this.getControllerFactory()
                 .getEliminarTemaController();
         eliminarTemaController.eliminarTema(id);
         LogManager.getLogger(this.getClass().getName()).info("--- Tema eliminado ---");
         return "home.xhtml";
     }
-
-
-
 
 }
